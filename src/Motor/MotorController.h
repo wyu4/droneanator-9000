81ccf30ee20186457e4ba31b0eed8196ce39640d
Wilson <wilsonyu657@gmail.com>
2025-11-03 07:19:13 -0500

    Correct mid comment

--- src/Motor/MotorController.h
+++ src/Motor/MotorController.h
@@ -4,7 +4,7 @@ const uint32_t PWM_FREQUENCY = 250; // Hz
 const uint32_t PWM_RESOLUTION = 12; // Bits, meaning a min of 0, and max of 2^(whatever this variable is set to)
 const int PWM_MIN = 0;              // Should be zero
 const int PWM_MAX = 4000;           // ~2 ^ PWM_RESOLUTION
-const int PWM_MID = 2000;           // (MAX - MIN)/2
+const int PWM_MID = 2000;           // (MAX + MIN) / 2
 const float PWM_MULTIPLIER = 1.024; // (2 ^ PWM_RESOLUTION) / PWM_MAX
 
 /**
--- src/Receiver/Receiver.cpp
+++ src/Receiver/Receiver.cpp
@@ -2,6 +2,10 @@
 #include <IBusBM.h>
 #include "Receiver.h"
 
+const int MIN_RATE = 1000; // Minimum output rate of the controller (microseconds)
+const int MAX_RATE = 2000; // Maximium output rate of the controller (microseconds)
+const int MID_RATE = 1500; // (MAX_RATE + MIN_RATE) / 2
+
 const uint8_t ROLL_CHANNEL = 1;
 const uint8_t PITCH_CHANNEL = 2;
 const uint8_t YAW_CHANNEL = 3;
